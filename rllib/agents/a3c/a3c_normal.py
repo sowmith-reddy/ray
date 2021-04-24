545694108d6a93ca88125ff1a4c4980c83995e2a
sowmith-reddy <raja@DESKTOP-TDH9IVD.localdomain>
2021-04-24 00:25:52 -0700

    Added code for hybrid of A3C and A2C in a3c.py file. Code for actual a3c can be found in a3c_normal.py

--- rllib/agents/a3c/a3c_normal.py
+++ rllib/agents/a3c/a3c_normal.py
@@ -3,18 +3,14 @@ import logging
 from ray.rllib.agents.a3c.a3c_tf_policy import A3CTFPolicy
 from ray.rllib.agents.trainer import with_common_config
 from ray.rllib.agents.trainer_template import build_trainer
-from ray.rllib.evaluation.worker_set import WorkerSet
 from ray.rllib.execution.rollout_ops import AsyncGradients
 from ray.rllib.execution.train_ops import ApplyGradients
 from ray.rllib.execution.metric_ops import StandardMetricsReporting
-from ray.rllib.execution.rollout_ops import ParallelRollouts, ConcatBatches
-from ray.rllib.execution.train_ops import ComputeGradients, AverageGradients, \
-    ApplyGradients, TrainTFMultiGPU, TrainOneStep
 
-logger = logging.getLogger(_name_)
+logger = logging.getLogger(__name__)
 
 # yapf: disable
-# _sphinx_doc_begin_
+# __sphinx_doc_begin__
 DEFAULT_CONFIG = with_common_config({
     # Should use a critic as a baseline (otherwise don't use value baseline;
     # required for using GAE).
@@ -42,7 +38,7 @@ DEFAULT_CONFIG = with_common_config({
     # rollout_fragment_length by up to 5x due to async buffering of batches.
     "sample_async": True,
 })
-# _sphinx_doc_end_
+# __sphinx_doc_end__
 # yapf: enable
 
 
@@ -61,62 +57,16 @@ def validate_config(config):
     if config["num_workers"] <= 0 and config["sample_async"]:
         raise ValueError("`num_workers` for A3C must be >= 1!")
 
-from itertools import chain
+
 def execution_plan(workers, config):
     # For A3C, compute policy gradients remotely on the rollout workers.
-    # rollouts = ParallelRollouts(workers, mode="bulk_sync")
-
     grads = AsyncGradients(workers)
-    
+
     # Apply the gradients as they arrive. We set update_all to False so that
     # only the worker sending the gradient is updated with new weights.
-    #train_op = grads.for_each(ApplyGradients(workers, update_all=False))
-    print("_____")
-    print(workers)
-    temp1 = workers
-    temp2 = workers
-    rem1 = workers.remote_workers()[0:6]
-    rem2 = workers.remote_workers()[6:11]
-    temp1.reset(rem1)
-    temp2.reset(rem2)
-  
-    rollouts1 = ParallelRollouts(temp1, mode="bulk_sync")
-    rollouts2 = ParallelRollouts(temp2, mode="bulk_sync")
+    train_op = grads.for_each(ApplyGradients(workers, update_all=False))
 
-
-    train_step_op1 = TrainTFMultiGPU(
-                workers=temp1,
-                sgd_minibatch_size=config["train_batch_size"],
-                num_sgd_iter=1,
-                num_gpus=config["num_gpus"],
-                shuffle_sequences=True,
-                _fake_gpus=config["_fake_gpus"],
-                framework=config.get("framework"))
-
-    train_step_op2 = TrainTFMultiGPU(
-                    workers=temp2,
-                    sgd_minibatch_size=config["train_batch_size"],
-                    num_sgd_iter=1,
-                    num_gpus=config["num_gpus"],
-                    shuffle_sequences=True,
-                    _fake_gpus=config["_fake_gpus"],
-                    framework=config.get("framework"))
-
-    train_op1 = rollouts1.combine(
-            ConcatBatches(
-                min_batch_size=config["train_batch_size"],
-                count_steps_by=config["multiagent"][
-                    "count_steps_by"])).for_each(train_step_op1)
-    train_op2 = rollouts2.combine(
-            ConcatBatches(
-                min_batch_size=config["train_batch_size"],
-                count_steps_by=config["multiagent"][
-                    "count_steps_by"])).for_each(train_step_op2)
-    
-    #train_op = grads.for_each(ApplyGradients(workers, update_all=False))
-    
-    
-    return StandardMetricsReporting(train_op1, temp1, config).union(StandardMetricsReporting(train_op2, temp2, config))
+    return StandardMetricsReporting(train_op, workers, config)
 
 
 A3CTrainer = build_trainer(
